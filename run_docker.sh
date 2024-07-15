493f541049e348dd56b6a57bd98aff39cc446d3a
jprogrammer13 <buss.riky@gmail.com>
2024-07-15 11:27:09 +0200

    ros2 humble description

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 xhost +
-sudo docker run --rm -it --name drp_docker --network host --ulimit nofile=1024:524288  --gpus all --workdir="/root" --env="DISPLAY=$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ./src:/root/ros_ws/src --device /dev/dri:/dev/dri  -e "QT_X11_NO_MITSHM=1" --shm-size 2g --rm drp_agilex_limo:latest /bin/bash
+sudo docker run --rm -it --name drp_docker --network host --ulimit nofile=1024:524288  --gpus all --workdir="/root" --env="DISPLAY=$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ./src:/root/ros2_ws/src -v ./gazebo_models:/root/.gazebo/models --device /dev/dri:/dev/dri  -e "QT_X11_NO_MITSHM=1" drp_agilex_limo:latest /bin/bash
