fixed trajectory velocity computation

tractor_simulator_multiple.py
@@ -280,7 +280,6 @@ class GenericSimulator(BaseController):
             plt.ylabel("angular velocity[rad/s]")
             plt.grid(True)
 
-
             # plotJoint('position', self.time_log, q_log=self.q_log, q_des_log=self.q_des_log, joint_names=self.joint_names)
             # joint velocities with limits
             plt.figure()
@@ -304,9 +303,8 @@ class GenericSimulator(BaseController):
             plt.ylabel("WHEEL_R")
             plt.grid(True)
 
-
             # states plot
-            #base position
+            # base position
             plotFrameLinear(name='position', title=f'{self.robot_name}', time_log=self.time_log,
                             des_Pose_log=self.des_state_log, Pose_log=self.state_log)
             # base velocity
@@ -518,7 +516,6 @@ class GenericSimulator(BaseController):
         # this is for getting the slipage
         return self.qd[0], self.qd[1], self.beta_l, self.beta_r, self.alpha
 
-
     def getGPSReading(self):
         return self.basePoseW + self.noise_pose, self.baseTwistW + self.noise_twist
 
@@ -556,9 +553,9 @@ def start_robots(robots, trajectory, groundMap):
 
     for robot in robots:
         if robot.DEBUG:
-            t += 1 
+            t += 1
         else:
-            t +=  np.random.randint(2, 5)
+            t += np.random.randint(5, 10)
         robot.t_start = t
 
         x, y, robot.old_theta, _, _, _, _ = trajectory.eval_trajectory(
@@ -573,6 +570,8 @@ def start_robots(robots, trajectory, groundMap):
 
         ground = groundMap.get_ground(x, y)
         robot.tracked_vehicle_simulator.setGround(ground)
+        # DEBUG
+        # robot.tracked_vehicle_simulator.NO_SLIPPAGE = True
 
         robot.initVars()
         robot.startupProcedure()
@@ -690,12 +689,13 @@ def talker(robots, trajectory, groundMap, data_path):
             if robot.DEBUG:
                 ground = Ground()
             else:
-                #update the ground according on the position
+                # update the ground according on the position
                 ground = groundMap.get_ground(
                     robot.robot_state.x, robot.robot_state.y)
             robot.tracked_vehicle_simulator.setGround(ground)
 
-            i, j = groundMap.coords_to_index(robot.robot_state.x, robot.robot_state.y)
+            i, j = groundMap.coords_to_index(
+                robot.robot_state.x, robot.robot_state.y)
 
             robot.des_x, robot.des_y, robot.des_theta, robot.v_d, robot.omega_d, robot.v_dot_d, robot.omega_dot_d = trajectory.eval_trajectory(
                 robot.time + robot.t_start)
@@ -737,8 +737,8 @@ def talker(robots, trajectory, groundMap, data_path):
                     'beta_l': [beta_l],
                     'beta_r': [beta_r],
                     'alpha': [alpha],
-                    'i' : [i],
-                    'j' : [j],
+                    'i': [i],
+                    'j': [j],
                 })
 
                 df = pd.concat([df, new_data], ignore_index=True)
@@ -782,7 +782,7 @@ if __name__ == '__main__':
 
     groundMap = GroundMap(6, 6)
     traj_viapoints = generate_circle_viapoints(2.5, 20)
-    traj_t_tot = 60
+    traj_t_tot = 50
     trajectory = LoopTrajectory(traj_viapoints, traj_t_tot)
 
     n_tracktors = 5  # with more than 3 it gets crazy
@@ -803,8 +803,5 @@ if __name__ == '__main__':
     ros.signal_shutdown("killed")
     for tracktor in tracktors:
         tracktor.deregister_node()
-    # save data
-
-    # print(f"Plotting data of {tracktor.robot_name}")
-    for i in range(n_tracktors):
-        tracktors[i].plotData()
+        # if tracktor.DEBUG:
+        tracktor.plotData()

trajectory.py
@@ -3,26 +3,29 @@ from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 import math
 
+
 class LoopTrajectory:
     def __init__(self, viapoints, t_tot):
         """
         Initialize with an array of viapoints. Viapoints should be a Nx2 array.
         """
         self.viapoints = np.array(viapoints)
-        # if the last and first viapoints does not match, append 
+        # if the last and first viapoints does not match, append
         if not np.allclose(self.viapoints[0], self.viapoints[-1]):
             self.viapoints = np.vstack([self.viapoints, self.viapoints[0]])
         self.t_tot = t_tot
         self.compute_spline(t_tot)
-    
+
     def compute_spline(self, t_tot):
         """
         Compute cubic spline for the given viapoints.
         """
         t = np.linspace(0, t_tot, len(self.viapoints))
-        self.spline_x = CubicSpline(t, self.viapoints[:, 0], bc_type='periodic')
-        self.spline_y = CubicSpline(t, self.viapoints[:, 1], bc_type='periodic')
-    
+        self.spline_x = CubicSpline(
+            t, self.viapoints[:, 0], bc_type='periodic')
+        self.spline_y = CubicSpline(
+            t, self.viapoints[:, 1], bc_type='periodic')
+
     def eval_trajectory(self, t):
         """
         Compute x, y, omega, x_dot, y_dot, omega_dot as a function of t.
@@ -32,29 +35,42 @@ class LoopTrajectory:
         x_dot = self.spline_x(t, 1)
         y_dot = self.spline_y(t, 1)
 
-        theta = math.atan2(y_dot.item(0), x_dot.item(0))
+        theta = np.arctan2(y_dot.item(0), x_dot.item(0))
 
-        v = np.linalg.norm(np.array([x_dot,y_dot]))
+        w_R_b = np.array([[np.cos(theta), -np.sin(theta)],
+                          [np.sin(theta), np.cos(theta)]])
+
+        b_vel_xy = (w_R_b.T).dot(np.array([x_dot, y_dot]))
+        # v = np.linalg.norm(b_vel_xy)
+
+        # only x because is the tangential part, y is the radial and is not 0, we assume unycycle so no radial part
+        v = b_vel_xy[0]
+
         x_ddot = self.spline_x(t, 2)
         y_ddot = self.spline_y(t, 2)
-        omega = (x_dot * y_ddot - y_dot * x_ddot) / (x_dot**2 + y_dot**2)
-        v_dot = np.linalg.norm(np.array([x_ddot,y_ddot]))
 
-        #TODO
+        omega = (x_dot * y_ddot - y_dot * x_ddot) / (x_dot**2 + y_dot**2)
+
+        b_vel_dot_xy = (w_R_b.T).dot(np.array([x_ddot, y_ddot]))
+        # v_dot = np.linalg.norm(b_vel_dot_xy)
+        v_dot = b_vel_dot_xy[0]
+
+        # TODO
         omega_dot = 0.
 
-        return x, y, theta, v, omega, omega_dot, v_dot
-    
+        return x, y, theta, v, omega, v_dot, omega_dot
+
     def plot_trajectory(self):
         """
         Plot the trajectory for visualization.
         """
         t = np.linspace(0, self.t_tot, 100)
-        x, y, _ , _ , _, _, _ = self.eval_trajectory(t)
-        
+        x, y, _, _, _, _, _ = self.eval_trajectory(t)
+
         plt.figure()
         plt.plot(x, y, label='Trajectory')
-        plt.plot(self.viapoints[:, 0], self.viapoints[:, 1], 'ro', label='Points')
+        plt.plot(self.viapoints[:, 0],
+                 self.viapoints[:, 1], 'ro', label='Points')
         plt.legend()
         plt.xlabel('X')
         plt.ylabel('Y')